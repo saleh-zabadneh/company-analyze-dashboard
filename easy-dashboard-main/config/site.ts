c6f5a1a1aa904e0b5b345734c04d27015d7bcab2
SalehZababneh <saleh.zabadneh@gmail.com>
2024-09-28 21:31:06 +0300

    Initial commit

---
+++ easy-dashboard-main/config/site.ts
@@ -0,0 +1,18 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "Easy Dashboard",
+  description:
+    "Beautifully designed templates built with Shadcn UI and Tailwind CSS.",
+  mainNav: [
+    {
+      title: "Home",
+      href: "/",
+    },
+  ],
+  links: {
+    twitter: "https://twitter.com/kathanmehtaa",
+    github: "https://github.com/DarkInventor/easy-ui",
+    docs: "https://easyui.pro",
+  },
+}
